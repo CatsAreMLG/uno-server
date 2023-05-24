acd0e01c8d692b8bada19b49e4f8068cc3ee6cc3
CatsAreMLG <joshuadgon@gmail.com>
2023-05-24 03:49:08 -0500

    Remove comments

--- server.js
+++ server.js
@@ -61,22 +61,15 @@ io.on("connection", (socket) => {
   })
 })
 
-//serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  //set static folder
-  // app.use(express.static("client/build"))
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  // })
+  app.get("/", (req, res) => {
+    res.status(200).send("Hello server is running").end()
+  })
+
+  app.get("/hello", (req, res) => {
+    res.send("Hello World!")
+  })
 }
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello server is running").end()
-})
-
-app.get("/hello", (req, res) => {
-  res.send("Hello World!")
-})
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
