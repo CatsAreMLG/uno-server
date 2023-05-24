37a35aab5a85552cb0612510bce5c70f5d47a05d
CatsAreMLG <joshuadgon@gmail.com>
2023-05-24 03:48:03 -0500

    Comment out client code

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ io.on("connection", (socket) => {
 //serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+  // app.use(express.static("client/build"))
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  // })
 }
 
 app.get("/", (req, res) => {
